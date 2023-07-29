855b8487204fd076968dd6d8942eac1134a97d06
Hasini Jeewanthi <vghasini234@gmail.com>
2023-07-30 01:06:28 +0530

    Complete Task 2

--- Demo2.java
+++ Demo2.java
@@ -1,4 +1,13 @@
 public class Demo2 {
+    public static void main(String[] args) {
+        final double pi = Math.PI;
+
+        System.out.printf("%.4f\n", pi);
+
+    }
     
+    
+    
+
     
 }
