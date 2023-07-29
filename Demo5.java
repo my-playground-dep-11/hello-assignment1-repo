866386984b143b8ac4c6c70b159f1e2d47844e92
Hasini Jeewanthi <vghasini234@gmail.com>
2023-07-30 03:03:49 +0530

    Complete Task 5

---
+++ Demo5.java
@@ -0,0 +1,18 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        String num1 = "978";
+        String num2 = "3";
+        String num3 = "16";
+        String num4 = "1484100";
+
+        final String COLOR1 = "\033[34m";
+
+        System.out.printf("%5$s%1$s-%2$s-%3$s-%4$s\n", num1, num2, num3, num4, COLOR1);
+
+
+        
+
+
+    }
+    
+}
