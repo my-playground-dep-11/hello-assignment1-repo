e203ebeac66007fa15350d8b9433e54b0a8767c8
Hasini Jeewanthi <vghasini234@gmail.com>
2023-07-30 00:44:31 +0530

    Complete Task 1

--- Demo1.java
+++ Demo1.java
@@ -1,6 +1,8 @@
 public class Demo1{
     public static void main(String[] args) {
-        
-        
+        float myBankBalance = 123456.7890f;
+
+        System.out.printf("$%,.2f\n", myBankBalance);
+
     }
 }
