0ce055ce05df1c8a1ae3de8b0b41d14473102b64
Hasini Jeewanthi <vghasini234@gmail.com>
2023-07-30 17:24:57 +0530

    Complete Task 6 - D(Modified)

--- Demo6D.java
+++ Demo6D.java
@@ -20,12 +20,17 @@ public class Demo6D{
         String double1 = "0.99";
         String double2 = "1.49";
 
+        String a = String.format("-".repeat(10));
+        String b = String.format("-".repeat(7));
 
-        System.out.printf("%1$s%2$s+--------------+----------+---------+%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET);
-        System.out.printf("%s%3$s| %2$s%4$-12s %3$s| %2$s%5$-8s %3$s| %2$s%6$-8s%3$s|%7$s\n", BACK_GROUND_COLOR, HEAD_LINE_COLOR, YELLOW_COLOR, str1, str2, str3, RESET);
-        System.out.printf("%1$s%2$s+%4$s--------------%2$s+----------+%4$s---------%2$s+%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET, GREEN_COLOR);
-        System.out.printf("%s%2$s| %3$s%1$s%7$-12s %2$s| %4$s%8$8s %2$s| %5$s%10$s%6$s%9$-6s %2$s|%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET, BLUE_COLOR, GREEN_COLOR, PINK_COLOR, str4, int1, double1, str6);
-        System.out.printf("%s%2$s| %3$s%1$s%7$-12s %2$s| %4$s%8$8s %2$s| %5$s%10$s%6$s%9$-6s %2$s|%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET, BLUE_COLOR, GREEN_COLOR, PINK_COLOR, str5, int2, double2, str6);
-        System.out.printf("%1$s%2$s+%4$s--------------%2$s+----------+%4$s---------%2$s+%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET, GREEN_COLOR);
+        //System.out.printf("%1$s%2$s+--------------+----------+---------+%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET);
+        System.out.printf("%1$s%2$s+%4$s+%4$s+%5$s+%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET, a, b);
+        System.out.printf("%s%3$s| %2$s%4$-8s %3$s| %2$s%5$-7s %3$s| %2$s%6$-6s%3$s|%7$s\n", BACK_GROUND_COLOR, HEAD_LINE_COLOR, YELLOW_COLOR, str1, str2, str3, RESET);
+        //System.out.printf("%1$s%2$s+%4$s--------------%2$s+----------+%4$s---------%2$s+%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET, GREEN_COLOR);
+        System.out.printf("%1$s%2$s+%4$s%5$s%2$s+%2$s%5$s+%4$s%6$s%2$s+%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET,GREEN_COLOR, a, b);
+        System.out.printf("%s%2$s| %3$s%1$s%7$-8s %2$s| %4$s%8$8s %2$s| %5$s%10$s%6$s%9$-4s %2$s|%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET, BLUE_COLOR, GREEN_COLOR, PINK_COLOR, str4, int1, double1, str6);
+        System.out.printf("%s%2$s| %3$s%1$s%7$-8s %2$s| %4$s%8$8s %2$s| %5$s%10$s%6$s%9$-4s %2$s|%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET, BLUE_COLOR, GREEN_COLOR, PINK_COLOR, str5, int2, double2, str6);
+        //System.out.printf("%1$s%2$s+%4$s--------------%2$s+----------+%4$s---------%2$s+%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET, GREEN_COLOR);
+        System.out.printf("%1$s%2$s+%4$s%5$s%2$s+%2$s%5$s+%4$s%6$s%2$s+%3$s\n", BACK_GROUND_COLOR, YELLOW_COLOR, RESET,GREEN_COLOR, a, b);
     }
 }
