1419ce4a5d5faf077f2ddb2ba7e97954b7f6f5ab
Hasini Jeewanthi <vghasini234@gmail.com>
2023-07-30 09:32:44 +0530

    Complete Task 6 - B

---
+++ Demo6B.java
@@ -0,0 +1,25 @@
+public class Demo6B {
+    public static void main(String[] args) {
+        final String COLOR1 = "\033[33m";
+        final String COLOR2 = "\033[34;1m";
+        final String COLOR3 = "\033[35m";
+        final String COLOR4 = "\033[40m";
+        final String RESET = "\033[30;0m";
+        String str1 = "NAME";
+        String str2 = "AGE";
+        String str3 = "Alice";
+        String str4 = "Bob";
+        String str5 = "24";
+        String str6 = "30";
+
+        System.out.printf("%2$s%1$s+-------------------------+-----------+%3$s\n", COLOR1, COLOR4, RESET);
+        System.out.printf("%6$s%3$s|    %4$s%1$s                 %3$s|   %4$s%2$s     %3$s|%5$s\n", str1, str2, COLOR1, COLOR2, RESET, COLOR4);
+        System.out.printf("%2$s%1$s+-------------------------+-----------+%3$s\n", COLOR1, COLOR4, RESET);
+        System.out.printf("%6$s%3$s|    %5$s%6$s%1$s                %3$s|   %4$s%2$s      %3$s|%5$s\n", str3, str5, COLOR1, COLOR3, RESET, COLOR4);
+        System.out.printf("%6$s%3$s|    %5$s%6$s%1$s                  %3$s|   %4$s%2$s      %3$s|%5$s\n", str4, str6, COLOR1, COLOR3, RESET, COLOR4);
+        System.out.printf("%2$s%1$s+-------------------------+-----------+\n", COLOR1, COLOR4, RESET);
+    
+
+    }
+    
+}
